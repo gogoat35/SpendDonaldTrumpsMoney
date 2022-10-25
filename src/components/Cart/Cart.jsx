b4cfdbba7bf8198d06130fd4008564ddc5560e12
soner_sonmez <ichbinsirpak@gmail.com>
2022-10-25 16:08:10 +0300

    Fixed Receipt section

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,23 +6,42 @@ export default function Cart() {
   return (
     <>
       <div className="cartContainer" hidden={cart.length <= 0}>
-        <div>
-          <h1 style={{ textAlign: "center" }}>Your Receipt</h1>
-          {cart.map((item) => (
-            <div key={item.id}>
-              <h4>
-                {item.name} X{item.amount}
-              </h4>
-              <NumericFormat
-                displayType="text"
-                thousandSeparator=","
-                value={item.amount * item.price}
-                renderText={(value) => (
-                  <label style={{ textAlign: "right" }}>${value}</label>
-                )}
-              />
+        <h1 style={{ padding: "15px 15px 25px" }}>Your Receipt</h1>
+        {cart.map((item) => (
+          <div className="items">
+            <div style={{ textAlign: "left", textOverflow: "ellipsis" }}>
+              {item.name}
             </div>
-          ))}
+            <div style={{ textAlign: "left" }}>X{item.amount}</div>
+            <NumericFormat
+              displayType="text"
+              thousandSeparator=","
+              value={item.amount * item.price}
+              renderText={(value) => (
+                <div style={{ textAlign: "right", color: "#24c486" }}>
+                  ${value}
+                </div>
+              )}
+            />
+          </div>
+        ))}
+        <div className="total">
+          <span>TOTAL</span>
+
+          <NumericFormat
+            displayType="text"
+            thousandSeparator=","
+            value={cart.reduce((acc, items) => {
+              return (
+                acc +
+                items.amount *
+                  cart.find((product) => product.id === items.id).price
+              );
+            }, 0)}
+            renderText={(value) => (
+              <div style={{ color: "#24c486" }}>${value}</div>
+            )}
+          />
         </div>
       </div>
     </>
