3ede99a3e7355d3d976fff70b61b80832cff7011
loganofc <127574271+gogoat35@users.noreply.github.com>
2024-01-22 19:13:51 -0600

    Update Billgates.jsx

--- src/components/Bill/Billgates.jsx
+++ src/components/Bill/Billgates.jsx
@@ -6,11 +6,11 @@ export default function Billgates() {
       <div className="billContainer">
         <img
           className="billgates"
-          src="https://neal.fun/spend/billgates.jpg"
+          src="https://cdn.discordapp.com/attachments/1160050072873095219/1199159979240865852/DonaldTrumpFace.jpg?ex=65c187c2&is=65af12c2&hm=68385763fbc6cb95533cbd0dccfd27296f05243cdcbd8d6f8947be09925079a4&"
           alt=""
         />
         <div>
-          <h1 className="spend">Spend Bill Gates' Money</h1>
+          <h1 className="spend">Spend Donald Trumps' Money</h1>
         </div>
       </div>
     </div>
